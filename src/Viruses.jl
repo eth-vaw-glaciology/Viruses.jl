dd3753e9701ba4ac3cd3f9f7c3257ceaba9dad03
Mauro Werder <mauro3@runbox.com>
2020-05-12 10:34:54 +0200

    Added docs

--- src/Viruses.jl
+++ src/Viruses.jl
@@ -1,8 +1,21 @@
 module Viruses
 export meltrate
 
+"""
+    meltrate(temp, ddf)
+
+Calculates the melt rate using temperature
+
+Input:
+- temp -- temperature [C]
+- ddf  -- melt factor [mm/h/C]
+
+Output:
+- meltrate [mm/h]
+"""
 function meltrate(temp, ddf)
     if temp<0
+        # it's freezing, no melt
         0.0
     elseif temp>0
         temp*ddf
